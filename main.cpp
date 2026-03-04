4bbc7c25a8f6912701288166f31bf93c3535554c
NirajMatai <matainiraj18@gmail.com>
2026-03-04 23:24:39 +0530

    Upgraded FEN parser to read game state variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <sstream>
 using namespace std;
 
 // --- PIECE & COLOR DEFINITIONS ---
@@ -11,22 +11,21 @@ const int BISHOP = 3;
 const int ROOK   = 4;
 const int QUEEN  = 5;
 const int KING   = 6;
-
 const int WHITE = 8;
 const int BLACK = 16;
 
 // --- THE BOARD & STATE ---
 int board[128];
-int sideToMove = WHITE; 
+int sideToMove = WHITE;
 int enPassantSquare = -1;
-int castleWK = 1, castleWQ = 1, castleBK = 1, castleBQ = 1; 
+int castleWK = 0, castleWQ = 0, castleBK = 0, castleBQ = 0;
 
 // --- MOVE OFFSETS ---
 int knightOffsets[8] = {33, 31, 18, 14, -14, -18, -31, -33};
 int kingOffsets[8]   = {17, 16, 15, 1, -1, -15, -16, -17};
 int bishopOffsets[4] = {17, 15, -15, -17};
-int rookOffsets[4]   = {16, 1, -1, -16};   
-int queenOffsets[8]  = {17, 16, 15, 1, -1, -15, -16, -17}; 
+int rookOffsets[4]   = {16, 1, -1, -16};
+int queenOffsets[8]  = {17, 16, 15, 1, -1, -15, -16, -17};
 
 void clearBoard() {
     for (int i = 0; i < 128; i++) {
@@ -34,20 +33,26 @@ void clearBoard() {
     }
 }
 
+// Upgraded FEN Parser
 void parseFEN(string fen) {
     clearBoard();
-    int rank = 7, file = 0;
+    castleWK = 0; castleWQ = 0; castleBK = 0; castleBQ = 0;
+    enPassantSquare = -1;
 
-    for (char const &c : fen) {
-        if (c == ' ') break; 
+    stringstream ss(fen);
+    string boardPart, colorPart, castlingPart, epPart;
+    ss >> boardPart >> colorPart >> castlingPart >> epPart;
+
+    // 1. Parse Pieces
+    int rank = 7, file = 0;
+    for (char const &c : boardPart) {
         if (c == '/') {
             rank--; file = 0;
         } else if (isdigit(c)) {
-            file += (c - '0'); 
+            file += (c - '0');
         } else {
             int square = rank * 16 + file;
             int piece = EMPTY;
-
             if (c == 'P') piece = WHITE | PAWN;
             else if (c == 'N') piece = WHITE | KNIGHT;
             else if (c == 'B') piece = WHITE | BISHOP;
@@ -60,10 +65,30 @@ void parseFEN(string fen) {
             else if (c == 'r') piece = BLACK | ROOK;
             else if (c == 'q') piece = BLACK | QUEEN;
             else if (c == 'k') piece = BLACK | KING;
-
             board[square] = piece;
             file++;
         }
+    }
+
+    // 2. Parse Side to Move
+    if (colorPart == "w") sideToMove = WHITE;
+    else sideToMove = BLACK;
+
+    // 3. Parse Castling Rights
+    if (castlingPart != "-") {
+        for (char const &c : castlingPart) {
+            if (c == 'K') castleWK = 1;
+            else if (c == 'Q') castleWQ = 1;
+            else if (c == 'k') castleBK = 1;
+            else if (c == 'q') castleBQ = 1;
+        }
+    }
+
+    // 4. Parse En Passant Square
+    if (epPart != "-") {
+        int epFile = epPart[0] - 'a';
+        int epRank = epPart[1] - '1';
+        enPassantSquare = epRank * 16 + epFile;
     }
 }
 
@@ -111,26 +136,20 @@ string squareToAlgebraic(int sq) {
 
 void generateMoves() {
     cout << "--- GENERATING MOVES FOR " << (sideToMove == WHITE ? "WHITE" : "BLACK") << " ---\n";
-    
     for (int square = 0; square < 128; square++) {
         if (square & 0x88) continue;
-        
         int piece = board[square];
         if (piece == EMPTY) continue;
-        
         int pieceColor = piece & (WHITE | BLACK);
         if (pieceColor != sideToMove) continue;
+        int pieceType = piece & 7;
 
-        int pieceType = piece & 7; 
-        
         // --- PAWNS ---
         if (pieceType == PAWN) {
             if (sideToMove == WHITE) {
-                // Single Push
                 int target = square + 16;
                 if ((target & 0x88) == 0 && board[target] == EMPTY) {
                     cout << "Pawn on " << squareToAlgebraic(square) << " pushes to " << squareToAlgebraic(target) << "\n";
-                    // Double Push (Only if on Rank 2, meaning array index is between 16 and 23)
                     if (square >= 16 && square <= 23) {
                         int doubleTarget = square + 32;
                         if ((doubleTarget & 0x88) == 0 && board[doubleTarget] == EMPTY) {
@@ -138,8 +157,6 @@ void generateMoves() {
                         }
                     }
                 }
-                
-                // Captures
                 int capLeft = square + 15;
                 if ((capLeft & 0x88) == 0 && board[capLeft] != EMPTY && (board[capLeft] & BLACK)) {
                     cout << "Pawn on " << squareToAlgebraic(square) << " captures on " << squareToAlgebraic(capLeft) << "\n";
@@ -148,13 +165,10 @@ void generateMoves() {
                 if ((capRight & 0x88) == 0 && board[capRight] != EMPTY && (board[capRight] & BLACK)) {
                     cout << "Pawn on " << squareToAlgebraic(square) << " captures on " << squareToAlgebraic(capRight) << "\n";
                 }
-            } 
-            else { // BLACK PAWNS
-                // Single Push
+            } else { 
                 int target = square - 16;
                 if ((target & 0x88) == 0 && board[target] == EMPTY) {
                     cout << "Pawn on " << squareToAlgebraic(square) << " pushes to " << squareToAlgebraic(target) << "\n";
-                    // Double Push (Only if on Rank 7, meaning array index is between 96 and 103)
                     if (square >= 96 && square <= 103) {
                         int doubleTarget = square - 32;
                         if ((doubleTarget & 0x88) == 0 && board[doubleTarget] == EMPTY) {
@@ -162,8 +176,6 @@ void generateMoves() {
                         }
                     }
                 }
-                
-                // Captures
                 int capLeft = square - 17;
                 if ((capLeft & 0x88) == 0 && board[capLeft] != EMPTY && (board[capLeft] & WHITE)) {
                     cout << "Pawn on " << squareToAlgebraic(square) << " captures on " << squareToAlgebraic(capLeft) << "\n";
@@ -174,8 +186,7 @@ void generateMoves() {
                 }
             }
         }
-        
-        // --- LEAPERS (Knight & King) ---
+        // --- LEAPERS ---
         else if (pieceType == KNIGHT || pieceType == KING) {
             int* offsets = (pieceType == KNIGHT) ? knightOffsets : kingOffsets;
             for (int i = 0; i < 8; i++) {
@@ -191,22 +202,18 @@ void generateMoves() {
                 }
             }
         }
-        
-        // --- SLIDERS (Bishop, Rook, Queen) ---
+        // --- SLIDERS ---
         else if (pieceType == BISHOP || pieceType == ROOK || pieceType == QUEEN) {
             int* offsets;
             int numDirections;
-
             if (pieceType == BISHOP) { offsets = bishopOffsets; numDirections = 4; }
             else if (pieceType == ROOK) { offsets = rookOffsets; numDirections = 4; }
-            else { offsets = queenOffsets; numDirections = 8; } 
-
+            else { offsets = queenOffsets; numDirections = 8; }
             for (int i = 0; i < numDirections; i++) {
                 int target = square + offsets[i];
                 while ((target & 0x88) == 0) {
                     int targetPiece = board[target];
                     int targetColor = targetPiece & (WHITE | BLACK);
-
                     if (targetPiece == EMPTY) {
                         cout << getPieceChar(piece) << " on " << squareToAlgebraic(square) << " slides to " << squareToAlgebraic(target) << "\n";
                     } else if (targetColor != sideToMove) {
@@ -224,11 +231,11 @@ void generateMoves() {
 }
 
 int main() {
-    parseFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    // Test string: Black's turn, missing queenside castling, en passant target on e6 (index 84)
+    parseFEN("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b Kk e6 0 1");
     printBoard();
     printGameState();
-    
     generateMoves();
-    
+
     return 0;
 }
