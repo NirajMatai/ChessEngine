234b9a1fa37a14970eceffc6dbe828730e0abd27
NirajMatai <matainiraj18@gmail.com>
2026-03-07 13:49:52 +0530

    Fixed GUI output buffering using endl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 // --- PIECE & COLOR DEFINITIONS ---
@@ -28,9 +31,7 @@ int rookOffsets[4]   = {16, 1, -1, -16};
 int queenOffsets[8]  = {17, 16, 15, 1, -1, -15, -16, -17};
 
 void clearBoard() {
-    for (int i = 0; i < 128; i++) {
-        board[i] = EMPTY;
-    }
+    for (int i = 0; i < 128; i++) board[i] = EMPTY;
 }
 
 void parseFEN(string fen) {
@@ -51,32 +52,23 @@ void parseFEN(string fen) {
         } else {
             int square = rank * 16 + file;
             int piece = EMPTY;
-            if (c == 'P') piece = WHITE | PAWN;
-            else if (c == 'N') piece = WHITE | KNIGHT;
-            else if (c == 'B') piece = WHITE | BISHOP;
-            else if (c == 'R') piece = WHITE | ROOK;
-            else if (c == 'Q') piece = WHITE | QUEEN;
-            else if (c == 'K') piece = WHITE | KING;
-            else if (c == 'p') piece = BLACK | PAWN;
-            else if (c == 'n') piece = BLACK | KNIGHT;
-            else if (c == 'b') piece = BLACK | BISHOP;
-            else if (c == 'r') piece = BLACK | ROOK;
-            else if (c == 'q') piece = BLACK | QUEEN;
-            else if (c == 'k') piece = BLACK | KING;
+            if (c == 'P') piece = WHITE | PAWN; else if (c == 'N') piece = WHITE | KNIGHT;
+            else if (c == 'B') piece = WHITE | BISHOP; else if (c == 'R') piece = WHITE | ROOK;
+            else if (c == 'Q') piece = WHITE | QUEEN; else if (c == 'K') piece = WHITE | KING;
+            else if (c == 'p') piece = BLACK | PAWN; else if (c == 'n') piece = BLACK | KNIGHT;
+            else if (c == 'b') piece = BLACK | BISHOP; else if (c == 'r') piece = BLACK | ROOK;
+            else if (c == 'q') piece = BLACK | QUEEN; else if (c == 'k') piece = BLACK | KING;
             board[square] = piece;
             file++;
         }
     }
 
-    if (colorPart == "w") sideToMove = WHITE;
-    else sideToMove = BLACK;
+    if (colorPart == "w") sideToMove = WHITE; else sideToMove = BLACK;
 
     if (castlingPart != "-") {
         for (char const &c : castlingPart) {
-            if (c == 'K') castleWK = 1;
-            else if (c == 'Q') castleWQ = 1;
-            else if (c == 'k') castleBK = 1;
-            else if (c == 'q') castleBQ = 1;
+            if (c == 'K') castleWK = 1; else if (c == 'Q') castleWQ = 1;
+            else if (c == 'k') castleBK = 1; else if (c == 'q') castleBQ = 1;
         }
     }
 
@@ -115,9 +107,8 @@ void printBoard() {
 void printGameState() {
     cout << "Side to move: " << (sideToMove == WHITE ? "White" : "Black") << endl;
     cout << "En Passant Square: " << (enPassantSquare == -1 ? "None" : to_string(enPassantSquare)) << endl;
-    cout << "Castling Rights: ";
-    cout << (castleWK ? "K" : "-"); cout << (castleWQ ? "Q" : "-");
-    cout << (castleBK ? "k" : "-"); cout << (castleBQ ? "q" : "-") << endl << endl;
+    cout << "Castling Rights: " << (castleWK ? "K" : "-") << (castleWQ ? "Q" : "-") 
+         << (castleBK ? "k" : "-") << (castleBQ ? "q" : "-") << endl << endl;
 }
 
 string squareToAlgebraic(int sq) {
@@ -129,22 +120,17 @@ string squareToAlgebraic(int sq) {
     return s;
 }
 
-// --- PHASE 3, STEP 2: PARSING MOVES ---
-
-// Helper to convert "e2" into array index
 int algebraicToSquare(string alg) {
     int file = alg[0] - 'a';
     int rank = alg[1] - '1';
     return rank * 16 + file;
 }
 
-// Physically updates the C++ array based on the text move
 void makeMove(string moveStr) {
     int src = algebraicToSquare(moveStr.substr(0, 2));
     int tgt = algebraicToSquare(moveStr.substr(2, 2));
     int piece = board[src];
 
-    // Handle promotions sent by GUI (e.g., e7e8q)
     if (moveStr.length() == 5) {
         char promo = moveStr[4];
         int color = piece & (WHITE | BLACK);
@@ -154,30 +140,95 @@ void makeMove(string moveStr) {
         else if (promo == 'n') piece = color | KNIGHT;
     }
 
-    // Move the piece
     board[tgt] = piece;
     board[src] = EMPTY;
-
-    // Swap turns
     sideToMove = (sideToMove == WHITE) ? BLACK : WHITE;
 }
 
-// --- PHASE 3, STEP 1 & 2: THE UCI LOOP ---
+// --- PHASE 3, STEP 3: THE RANDOM MOVER ---
+// Instead of void, it now returns a list of UCI move strings
+vector<string> generateMoves() {
+    vector<string> moves;
+
+    for (int square = 0; square < 128; square++) {
+        if (square & 0x88) continue;
+        int piece = board[square];
+        if (piece == EMPTY) continue;
+        int pieceColor = piece & (WHITE | BLACK);
+        if (pieceColor != sideToMove) continue;
+        int pieceType = piece & 7;
+
+        // A quick lambda helper to build the UCI string and auto-promote to Queen
+        auto addMove = [&](int targetSquare) {
+            string m = squareToAlgebraic(square) + squareToAlgebraic(targetSquare);
+            if (pieceType == PAWN && (targetSquare >= 112 || targetSquare <= 7)) m += "q";
+            moves.push_back(m);
+        };
+
+        if (pieceType == PAWN) {
+            if (sideToMove == WHITE) {
+                int target = square + 16;
+                if ((target & 0x88) == 0 && board[target] == EMPTY) {
+                    addMove(target);
+                    if (square >= 16 && square <= 23 && board[square + 32] == EMPTY) addMove(square + 32);
+                }
+                if (((square + 15) & 0x88) == 0 && ((board[square + 15] != EMPTY && (board[square + 15] & BLACK)) || square + 15 == enPassantSquare)) addMove(square + 15);
+                if (((square + 17) & 0x88) == 0 && ((board[square + 17] != EMPTY && (board[square + 17] & BLACK)) || square + 17 == enPassantSquare)) addMove(square + 17);
+            } else { 
+                int target = square - 16;
+                if ((target & 0x88) == 0 && board[target] == EMPTY) {
+                    addMove(target);
+                    if (square >= 96 && square <= 103 && board[square - 32] == EMPTY) addMove(square - 32);
+                }
+                if (((square - 17) & 0x88) == 0 && ((board[square - 17] != EMPTY && (board[square - 17] & WHITE)) || square - 17 == enPassantSquare)) addMove(square - 17);
+                if (((square - 15) & 0x88) == 0 && ((board[square - 15] != EMPTY && (board[square - 15] & WHITE)) || square - 15 == enPassantSquare)) addMove(square - 15);
+            }
+        }
+        else if (pieceType == KNIGHT || pieceType == KING) {
+            int* offsets = (pieceType == KNIGHT) ? knightOffsets : kingOffsets;
+            for (int i = 0; i < 8; i++) {
+                int target = square + offsets[i];
+                if ((target & 0x88) == 0) {
+                    int targetColor = board[target] & (WHITE | BLACK);
+                    if (board[target] == EMPTY || targetColor != sideToMove) addMove(target);
+                }
+            }
+        }
+        else if (pieceType == BISHOP || pieceType == ROOK || pieceType == QUEEN) {
+            int* offsets; int numDirections;
+            if (pieceType == BISHOP) { offsets = bishopOffsets; numDirections = 4; }
+            else if (pieceType == ROOK) { offsets = rookOffsets; numDirections = 4; }
+            else { offsets = queenOffsets; numDirections = 8; }
+            for (int i = 0; i < numDirections; i++) {
+                int target = square + offsets[i];
+                while ((target & 0x88) == 0) {
+                    int targetColor = board[target] & (WHITE | BLACK);
+                    if (board[target] == EMPTY) { addMove(target); } 
+                    else if (targetColor != sideToMove) { addMove(target); break; } 
+                    else { break; }
+                    target += offsets[i];
+                }
+            }
+        }
+    }
+    return moves;
+}
+
 void uciLoop() {
     string line;
-    cout << "Engine is alive. Type 'uci' to begin.\n";
+    srand(time(0)); 
+    cout << "Engine is alive. Type 'uci' to begin." << endl;
 
     while (getline(cin, line)) {
         if (line == "uci") {
-            cout << "id name TheGreatSage\n";
-            cout << "id author You\n";
-            cout << "uciok\n";
+            cout << "id name TheGreatSage" << endl;
+            cout << "id author You" << endl;
+            cout << "uciok" << endl;
         } 
         else if (line == "isready") {
-            cout << "readyok\n";
+            cout << "readyok" << endl;
         } 
         else if (line.substr(0, 8) == "position") {
-            // Setup the board state
             if (line.find("startpos") != string::npos) {
                 parseFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
             } else if (line.find("fen") != string::npos) {
@@ -187,19 +238,24 @@ void uciLoop() {
                 parseFEN(fen);
             }
 
-            // If the GUI sent a list of moves, play them on our array
             size_t movesStart = line.find("moves ");
             if (movesStart != string::npos) {
                 string movesStr = line.substr(movesStart + 6);
                 stringstream ss(movesStr);
                 string singleMove;
-                while (ss >> singleMove) {
-                    makeMove(singleMove);
-                }
+                while (ss >> singleMove) makeMove(singleMove);
+            }
+        }
+        else if (line.substr(0, 2) == "go") {
+            vector<string> moves = generateMoves();
+            if (moves.size() > 0) {
+                int randomIndex = rand() % moves.size();
+                cout << "bestmove " << moves[randomIndex] << endl;
+            } else {
+                cout << "bestmove 0000" << endl; 
             }
         }
         else if (line == "d") {
-            // Custom debug command to see what the engine sees
             printBoard();
             printGameState();
         }
@@ -210,6 +266,8 @@ void uciLoop() {
 }
 
 int main() {
+    // Force standard output to not buffer at all, just to be bulletproof
+    setvbuf(stdout, NULL, _IONBF, 0); 
     uciLoop();
     return 0;
 }
